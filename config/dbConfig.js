67bc0027e66af7a6bb9c4471aa17199513279a41
Kurniawan Try Yudha <86092782+ktyudha@users.noreply.github.com>
2024-01-02 09:31:29 +0700

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "127.0.0.1",
-  USER: "ktyudha",
-  PASSWORD: "calonmertua2002",
-  DB: "dbefrisagcs",
+  USER: "efrisa",
+  PASSWORD: "",
+  DB: "",
   dialect: "postgres",
   pool: {
     max: 5,
